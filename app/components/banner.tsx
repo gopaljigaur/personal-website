9ef3703eb0a8109acff61f8be24e48d3fd0ce10e
Gopalji Gaur <gopaljigaur@gmail.com>
2025-02-07 23:08:02 +0100

    alternate layout

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -1,4 +1,37 @@
 import Image from "next/image";
+import Link from "next/link";
+
+export function MainBanner() {
+    return (
+        <span className="flex mb-14 dark:text-neutral-100 text-neutral-950 gap-2 flex-col-reverse sm:flex-row">
+            <div className="flex-row flex-2">
+                <Link href={"https://gopalji.me"}>
+                    <h1 className="text-5xl font-semibold relative tracking-tight">
+                        Gopalji Gaur
+                    </h1>
+                </Link>
+                <h2 className="text-2xl tracking-tight mt-6">
+                    <p className="font-semibold bg-gradient-to-tr from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text leading-normal">
+                        Machine Learning Engineer
+                    </p>
+                    <br></br>
+                    based in
+                    Freiburg, Germany.
+                </h2>
+            </div>
+            <div className="flex-1 content-center min-w-[128px] justify-items-start sm:justify-items-end mb-4 sm:mb-0">
+                <Image
+                    alt='Gopalji Gaur'
+                    height={128}
+                    width={128}
+                    priority={true}
+                    src='https://www.gravatar.com/avatar/0d634a9edf65bcc4916888473f10b1e6?size=200'
+                    className="rounded-full bg-neutral-200 dark:bg-neutral-800 dark:brightness-90 dark:saturate-[0.85] transition"
+                />
+            </div>
+        </span>
+    );
+}
 
 export function Banner(props) {
     return (
@@ -10,7 +43,7 @@ export function Banner(props) {
                     width={48}
                     priority={true}
                     src='https://www.gravatar.com/avatar/0d634a9edf65bcc4916888473f10b1e6?size=200'
-                    className="rounded-full bg-gray-200 dark:bg-gray-800 dark:brightness-90 dark:saturate-[0.85] transition"
+                    className="rounded-full bg-neutral-200 dark:bg-neutral-800 dark:brightness-90 dark:saturate-[0.85] transition"
                 />
             </div>
             <h1 className="mt-2.5 ml-5 text-2xl font-semibold relative tracking-tight">
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { BlogPosts } from 'app/components/posts'
-import { Banner } from 'app/components/banner'
+import { MainBanner } from 'app/components/banner'
+import { StatusCard } from 'app/components/status'
 
 export default function Page() {
   return (
       <section>
-          <Banner>gopaljigaur</Banner>
+          <MainBanner />
+          <StatusCard />
           <p className="mb-4">
               {`Hey, I'm Gopalji, a Machine Learning Engineer based in Germany. In addition to dwelling 
         among the interconnections of the Neural Networks, I sometimes like to wrap my head around 
